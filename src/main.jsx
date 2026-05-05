b53a0892cc37f45f92b84be5cd1aa0049fca2600
rebeccalword-lhp <rebeccalword@gmail.com>
2026-05-05 10:31:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LHPApp from './App'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LHPApp />
+  </React.StrictMode>
+)
