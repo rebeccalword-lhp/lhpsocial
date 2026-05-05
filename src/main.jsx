cc36e1441033b56cb2fb6d160d8fe1f4a0eb3ecd
rebeccalword-lhp <rebeccalword@gmail.com>
2026-05-05 10:37:43 -0400

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import LHPApp from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LHPApp />
-  </React.StrictMode>
-)
